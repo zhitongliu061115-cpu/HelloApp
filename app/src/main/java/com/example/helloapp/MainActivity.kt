c094cb4db685b9fe15dc8b7075c32ee332f139f8
Yeah <3790764375@qq.com>
2026-01-21 13:47:44 +0800

    zhw测试

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloAppTheme {
-        Greeting("yxzdtc")
+        Greeting("yxzdtc,也不一定")
     }
 }
