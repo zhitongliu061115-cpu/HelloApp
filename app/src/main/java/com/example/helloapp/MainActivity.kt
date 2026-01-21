a11912be0c9d72eb4f13ad3d57eac96a3f58b94c
X-Hbin <24301034@bjtu.edu.cn>
2026-01-21 14:05:15 +0800

    latest

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloAppTheme {
-        Greeting("hxbddddtc,一定")
+        Greeting("hxbdddddtc,一定")
     }
 }
